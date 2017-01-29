904a2e9a44bcb08de157e02f6dcef8280ede5bad
Daniel B <danyabessonov@hotmail.com>
2017-01-28 23:41:34 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ for j in range(0,16):
     for i in range(0,100):
         if ((j*100)+i) >0:
             if ((j*100)+i) < 2:
-                html = scraperwiki.scrape("http://www.crunchbase.com/funding-rounds")
+                html = scraperwiki.scrape("https://www.crunchbase.com/funding-rounds")
                 root = lxml.html.fromstring(html)
                 for tr in root.cssselect("div#col2_internal tr"):
                     tds = tr.cssselect("td")
